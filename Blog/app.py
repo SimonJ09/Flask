ba39acc3e9f8e54b7343794ab48e9b4ece274742
jude <jude-simon.agohoundje@epitech.eu>
2023-09-09 16:46:46 +0100

    ajout apli

--- Blog/app.py
+++ Blog/app.py
@@ -1,6 +1,17 @@
+
 from flask import Flask, render_template
+from flask import send_from_directory
 from db import Post
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder='stactic')
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///C:/Users/AGOHOUNDJE/Documents/Jude/Flask/Blog/database/db.sqlite3'
+db = SQLAlchemy(app)
+ 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
 
 @app.route('/')
@@ -23,7 +34,6 @@ def recherche():
 @app.route('/blog')
 def post_index():
     posts=Post.all()
-
     return render_template('posts/index.html',posts=posts)
 
 
@@ -37,5 +47,14 @@ def education():
     return render_template('pages/education.html')
 
 
+
+def index():
+    with app.app_context():
+        # You can now safely use the 'db' object within this context
+        result = YourModel.query.first()
+        return str(result)
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()    
     app.run(debug=True, port=3000)
