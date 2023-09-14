f2a75612a65617d664b6b59cbad171bd2af8bc6f
jude <jude-simon.agohoundje@epitech.eu>
2023-09-15 00:22:54 +0100

    ajout aplicationno start sh requierements

--- Blog/start.sh
+++ Blog/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-rm -r .venv  # Supprimer l'environnement virtuel existant
-python -m venv .venv  # Recréer l'environnement virtuel
-.venv\Scripts\activate
-
-
-pip install --upgrade pip
-pip install --upgrade -r requirements.txt
+python -m venv Flaskapp
+.\Flaskapp\Scripts\activate
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
 python app.py
