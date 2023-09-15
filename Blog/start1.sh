68d4769c2d48bf40200e9fd3d79757bef75c88c1
jude <jude-simon.agohoundje@epitech.eu>
2023-09-15 11:52:10 +0100

    message add  start1 redemarer

--- Blog/start1.sh
+++ Blog/start1.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-python -m venv Flaskapp
-.\Flaskapp\Scripts\activate
-python.exe -m pip install --upgrade pip
-pip install --upgrade pip
-gunicorn -c gunicorn_config.py app:app1
+
+# Créez un environnement virtuel Flask s'il n'existe pas
+if [ ! -d "Flaskapp" ]; then
+    python -m venv Flaskapp
+fi
+
+# Activez l'environnement virtuel Flask
+source Flaskapp/Scripts/activate
+
+# Mise à jour de pip dans l'environnement virtuel
+python -m pip install --upgrade pip
+
+# Installez les dépendances de votre application Flask (vous devez spécifier le chemin complet)
+pip install -r requirements.txt
+
+# Lancement de l'application avec Gunicorn
+gunicorn -c gunicorn_config.py app:app
