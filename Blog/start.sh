33a27fafbad4a51ac3bf5a9f9f60d99ace0bc35b
jude <jude-simon.agohoundje@epitech.eu>
2023-09-15 11:11:49 +0100

    ajout aplicationno start sh requierements fichier gunicon proficle guni requirement

--- Blog/start.sh
+++ Blog/start.sh
@@ -2,5 +2,5 @@
 python -m venv Flaskapp
 .\Flaskapp\Scripts\activate
 python.exe -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip
 gunicorn -c gunicorn_config.py app:app
