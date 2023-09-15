73032c5980711ce919bce5976252ccb7c71773ab
jude <jude-simon.agohoundje@epitech.eu>
2023-09-15 11:41:32 +0100

    message add  start1

--- Blog/start1.sh
+++ Blog/start1.sh
@@ -3,4 +3,4 @@ python -m venv Flaskapp
 .\Flaskapp\Scripts\activate
 python.exe -m pip install --upgrade pip
 pip install --upgrade pip
-gunicorn -c gunicorn_config.py app:app
+gunicorn -c gunicorn_config.py app:app1
